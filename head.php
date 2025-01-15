d77af21030a2b350fc575f6daa70c62c3328ead5
Christian Kit <57896689+creative-chris@users.noreply.github.com>
2025-01-15 12:26:26 +0100

    Basic setup

---
+++ head.php
@@ -0,0 +1,26 @@
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0" />
+    
+    <!-- CSS section -->
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen"/>
+    <link href="responsive.css" type="text/css" rel="stylesheet" media="screen"/>
+    <link href="intro.css" type="text/css" rel="stylesheet" media="screen"/>
+
+    <!-- Manifest JSON for PWA -->
+    <!-- <link rel="manifest" href="manifest.json"/> -->
+
+    <!-- Font family -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200..800&display=swap" rel="stylesheet">
+
+    <!-- Site title & icon -->
+    <title>Enabling Technologies</title>
+    <link rel="icon" href="Logo/makam-logo_black-icon.svg">
+
+    <!-- Javascript section -->
+    <script src="js/jquery-3.7.1.min.js"></script> 
+    <script src="animation.js" defer></script>
+    <script src="js/error.js" defer></script>
+</head>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<?php include("head.php"); ?>
+<body>
+    <script>
+        // if ('serviceWorker' in navigator) {
+        //     navigator.serviceWorker.register('service-worker.js')
+        // }
+    </script>
+    <div id="container">
+        <header></header>
+        <main>
+            <?php include("-"); ?>
+            <?php include("-"); ?>
+            <?php include("-"); ?>
+        </main>
+        <footer></footer>
+    </div>
+</body>
+</html>

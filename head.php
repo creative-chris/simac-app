ad649d3272c17afdfa586aa9213cde5edc00aa92
Christian Kit <57896689+creative-chris@users.noreply.github.com>
2025-01-26 22:53:21 +0100

    quiz page setup

--- head.php
+++ head.php
@@ -18,7 +18,7 @@
     <!-- Font family -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200..800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
     <!-- Site title & icon -->
     <title>Simac IT NL</title>
---
+++ quiz.php
@@ -0,0 +1,8 @@
+<ul class="quizstructure">
+    <li>
+        <img src="image/components/quiz-components/quiz1.png" />
+    </li>
+    <li><h3>Is this something Simac does?</h3></li>
+    <li class="buttons"><img class="true-button" src="image/components/quiz-components/true.png" />
+    <img class="false-button" src="image/components/quiz-components/false.png" /></li>
+</ul>
